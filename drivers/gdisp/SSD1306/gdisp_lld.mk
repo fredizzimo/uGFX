f4b9f0bcfeb5a71cf5b46073cc30f6e496c23dc2
inmarket <andrewh@inmarket.com.au>
2013-10-21 17:12:48 +1000

    Convert SSD1306 driver to new driver format. This driver requires the new flush operation.

--- drivers/gdisp/SSD1306/gdisp_lld.mk
+++ drivers/gdisp/SSD1306/gdisp_lld.mk
@@ -1,5 +1,2 @@
-# List the required driver.
-GFXSRC += $(GFXLIB)/drivers/gdisp/SSD1306/gdisp_lld.c
-
-# Required include directories
 GFXINC += $(GFXLIB)/drivers/gdisp/SSD1306
+GFXSRC += $(GFXLIB)/drivers/gdisp/SSD1306/gdisp_lld.c
