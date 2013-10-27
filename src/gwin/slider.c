cf616ae80412f20ddf0a534284b67010b4937969
Joel Bodenmann <joel@unormal.org>
2013-10-28 00:35:08 +0100

    Bugfix... how could we miss this?

--- src/gwin/slider.c
+++ src/gwin/slider.c
@@ -322,6 +322,7 @@ void gwinSliderDraw_Std(GWidgetObject *gw, void *param) {
 	#undef gsw
 }
 
+#if GDISP_NEED_IMAGE || defined(__DOXYGEN__)
 void gwinSliderDraw_Image(GWidgetObject *gw, void *param) {
 	#define gsw			((GSliderObject *)gw)
 	#define gi			((gdispImage *)param)
@@ -370,6 +371,7 @@ void gwinSliderDraw_Image(GWidgetObject *gw, void *param) {
 
 	#undef gsw
 }
+#endif /* GDISP_NEED_IMAGE */
 
 #endif /* GFX_USE_GWIN && GWIN_NEED_BUTTON */
 /** @} */
