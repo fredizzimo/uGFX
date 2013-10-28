1e9dcc4a286a17abf48703a632c2a3a42c856567
Joel Bodenmann <joel@unormal.org>
2013-10-28 01:00:02 +0100

    uncommented GOS selection in GDISP and GWIN demos

--- demos/modules/gwin/radio/gfxconf.h
+++ demos/modules/gwin/radio/gfxconf.h
@@ -30,11 +30,11 @@
 #ifndef _GFXCONF_H
 #define _GFXCONF_H
 
-/* The operating system to use - one of these must be defined */
-#define GFX_USE_OS_CHIBIOS		FALSE
-#define GFX_USE_OS_WIN32		FALSE
-#define GFX_USE_OS_LINUX		FALSE
-#define GFX_USE_OS_OSX			FALSE
+/* The operating system to use. One of these must be defined - perferably in your Makefile */
+//#define GFX_USE_OS_CHIBIOS		FALSE
+//#define GFX_USE_OS_WIN32		FALSE
+//#define GFX_USE_OS_LINUX		FALSE
+//#define GFX_USE_OS_OSX			FALSE
 
 /* GFX sub-systems to turn on */
 #define GFX_USE_GDISP			TRUE
