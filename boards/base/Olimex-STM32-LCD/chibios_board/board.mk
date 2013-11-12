91bcb0d41d74392e20be7fa26ba3d7a28c0ba240
Joel Bodenmann <joel@unormal.org>
2013-11-12 01:29:14 +0100

    Olimex-STM32-LCD board file fix

--- boards/base/Olimex-STM32-LCD/chibios_board/board.mk
+++ boards/base/Olimex-STM32-LCD/chibios_board/board.mk
@@ -1,5 +1,3 @@
-# List of all the board related files.
-BOARDSRC = ${CHIBIOS}/boards/OLIMEX_STM32_LCD/board.c
+BOARDINC = $(GFXLIB)/boards/base/Olimex-STM32-LCD/chibios_board
+BOARDSRC = $(BOARDINC)/board.c \
 
-# Required include directories
-BOARDINC = ${CHIBIOS}/boards/OLIMEX_STM32_LCD
