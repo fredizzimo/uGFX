a2df8d4fe150c34d080aee0ce0f2afc5436902d4
Joel Bodenmann <joel@unormal.org>
2013-11-12 01:20:25 +0100

    FireBull-STM32F103-FB board file fix

--- boards/base/FireBull-STM32F103-FB/chibios_board/board.mk
+++ boards/base/FireBull-STM32F103-FB/chibios_board/board.mk
@@ -1,6 +1,3 @@
-# List of all the board related files.
-BOARDSRC = ${CHIBIOS}/boards/FIREBULL_STM32_F103/board.c
-
-# Required include directories
-BOARDINC = ${CHIBIOS}/boards/FIREBULL_STM32_F103
+BOARDINC = $(GFXLIB)/boards/base/FireBull-STM32F103-FB/chibios_board
+BOARDSRC = $(BOARDINC)/board.c \
 
