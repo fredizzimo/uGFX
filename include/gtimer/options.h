e85780bc3fba26e34c1f3c72a5663302739b4077
inmarket <andrewh@inmarket.com.au>
2013-12-21 15:14:17 +1000

    Increase the default stack size for the GTimer thread as for widgets demo under Win32 1024 bytes is not enough.

--- include/gtimer/options.h
+++ include/gtimer/options.h
@@ -38,7 +38,7 @@
 	 * @details	Defaults to 512 bytes
 	 */
 	#ifndef GTIMER_THREAD_WORKAREA_SIZE
-		#define GTIMER_THREAD_WORKAREA_SIZE		1024
+		#define GTIMER_THREAD_WORKAREA_SIZE		2048
 	#endif
 /** @} */
 
