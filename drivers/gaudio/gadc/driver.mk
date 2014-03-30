c16cb15ce9b5e4cc1a913def9614f8a587bc0868
inmarket <andrewh@inmarket.com.au>
2014-03-30 19:08:53 +1000

    Fix definition of when GADC is turned on for GADC audio driver

--- drivers/gaudio/gadc/driver.mk
+++ drivers/gaudio/gadc/driver.mk
@@ -5,5 +5,5 @@ GFXSRC += $(GFXLIB)/drivers/gaudio/gadc/gaudio_record_lld.c
 GFXINC += $(GFXLIB)/drivers/gaudio/gadc
 
 # Make sure the GADC sub-system is turned on
-GFXDEFS += -DGFX_USE_GADC=TRUE
+GFXDEFS += -DGFX_USE_GADC=GAUDIO_NEED_RECORD
 
