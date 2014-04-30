68f50a3fe978611b8ac3d78987bb3d9efc58f06d
inmarket <andrewh@inmarket.com.au>
2014-05-01 07:41:33 +1000

    Only require GTimer for the vs1053 driver if GAUDIO is turned on.

--- drivers/gaudio/vs1053/driver.mk
+++ drivers/gaudio/vs1053/driver.mk
@@ -4,4 +4,4 @@ GFXSRC += $(GFXLIB)/drivers/gaudio/vs1053/gaudio_play_lld.c
 # Required include directories
 GFXINC += $(GFXLIB)/drivers/gaudio/vs1053
 
-GFXDEFS += -DGFX_USE_GTIMER=TRUE
+GFXDEFS += -DGFX_USE_GTIMER=GFX_USE_GAUDIO
