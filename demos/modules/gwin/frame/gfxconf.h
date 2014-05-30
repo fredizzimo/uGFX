25ce0ed7d6217e6f355dd069bba2b3ca1b63b5e3
inmarket <andrewh@inmarket.com.au>
2014-05-30 12:45:48 +1000

    Remove superflous entries in demo gfxconf.h

--- demos/modules/gwin/frame/gfxconf.h
+++ demos/modules/gwin/frame/gfxconf.h
@@ -24,23 +24,16 @@
 //#define GFX_USE_OS_LINUX                           TRUE
 //#define GFX_USE_OS_OSX                             TRUE
 
-
 ///////////////////////////////////////////////////////////////////////////
 // GDISP                                                                 //
 ///////////////////////////////////////////////////////////////////////////
 #define GFX_USE_GDISP                                TRUE
 
-#define GDISP_NEED_AUTOFLUSH                         FALSE
-#define GDISP_NEED_TIMERFLUSH                        FALSE
-#define GDISP_NEED_VALIDATION                        TRUE
-#define GDISP_NEED_CLIP                              TRUE
 #define GDISP_NEED_TEXT                              TRUE
-    #define GDISP_NEED_ANTIALIAS                     TRUE
-    #define GDISP_NEED_UTF8                          TRUE
-    #define GDISP_NEED_TEXT_KERNING                  TRUE
+    //#define GDISP_NEED_ANTIALIAS                     TRUE			// Not needed for UI2 font
+    //#define GDISP_NEED_TEXT_KERNING                  TRUE			// Not needed for UI2 font
+    //#define GDISP_NEED_UTF8                          TRUE
     #define GDISP_INCLUDE_FONT_UI2                   TRUE
-#define GDISP_NEED_CIRCLE                            TRUE
-#define GDISP_NEED_CONVEX_POLYGON                    TRUE
 #define GDISP_NEED_MULTITHREAD                       TRUE
 
 #define GDISP_DEFAULT_ORIENTATION                    GDISP_ROTATE_LANDSCAPE
@@ -52,33 +45,11 @@
 
 #define GWIN_NEED_WINDOWMANAGER                      TRUE
 
-#define GWIN_NEED_CONSOLE                            TRUE
-    #define GWIN_CONSOLE_USE_HISTORY                 TRUE
-        #define GWIN_CONSOLE_HISTORY_AVERAGING       TRUE
-        #define GWIN_CONSOLE_HISTORY_ATCREATE        TRUE
-    #define GWIN_CONSOLE_ESCSEQ                      TRUE
-	#define GWIN_CONSOLE_USE_BASESTREAM              TRUE
-#define GWIN_NEED_GRAPH                              TRUE
-
 #define GWIN_NEED_WIDGET                             TRUE
-    #define GWIN_NEED_LABEL                          TRUE
-        #define GWIN_LABEL_ATTRIBUTE                 TRUE
     #define GWIN_NEED_BUTTON                         TRUE
-        #define GWIN_BUTTON_LAZY_RELEASE             TRUE
     #define GWIN_NEED_SLIDER                         TRUE
-    #define GWIN_NEED_CHECKBOX                       TRUE
-    #define GWIN_NEED_IMAGE                          FALSE
-        #define GWIN_NEED_IMAGE_ANIMATION            TRUE
-    #define GWIN_NEED_RADIO                          TRUE
-    #define GWIN_NEED_LIST                           TRUE
-        #define GWIN_NEED_LIST_IMAGES                FALSE
-    #define GWIN_NEED_PROGRESSBAR                    TRUE
-        #define GWIN_PROGRESSBAR_AUTO                TRUE
-	#define GWIN_FLAT_STYLING                        TRUE
 #define GWIN_NEED_CONTAINERS                         TRUE
-    #define GWIN_NEED_CONTAINER                      TRUE
     #define GWIN_NEED_FRAME                          TRUE
-
 
 ///////////////////////////////////////////////////////////////////////////
 // GEVENT                                                                //
@@ -94,17 +65,12 @@
 // GQUEUE                                                                //
 ///////////////////////////////////////////////////////////////////////////
 #define GFX_USE_GQUEUE                               TRUE
-
-#define GQUEUE_NEED_ASYNC                            TRUE
-#define GQUEUE_NEED_GSYNC                            TRUE
-#define GQUEUE_NEED_FSYNC                            FALSE
-#define GQUEUE_NEED_BUFFERS                          FALSE
+    #define GQUEUE_NEED_ASYNC                        TRUE
 
 ///////////////////////////////////////////////////////////////////////////
 // GINPUT                                                                //
 ///////////////////////////////////////////////////////////////////////////
 #define GFX_USE_GINPUT                               TRUE
-
-#define GINPUT_NEED_MOUSE                            TRUE
+    #define GINPUT_NEED_MOUSE                        TRUE
 
 #endif /* _GFXCONF_H */
