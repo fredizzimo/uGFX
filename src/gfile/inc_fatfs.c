d7129e6058803743c0b183b4ac6ed10ed00d3c34
Joel Bodenmann <joel@unormal.org>
2014-06-24 05:56:13 +0200

    adding fatfs directory

--- src/gfile/inc_fatfs.c
+++ src/gfile/inc_fatfs.c
@@ -12,4 +12,91 @@
 /********************************************************
  * The FAT file-system VMT
  ********************************************************/
-#error "GFILE: FATFS Not implemented yet"
+
+static bool_t fatfsDel(const char* fname);
+static bool_t fatfsExists(const char* fname);
+static long int fatfsFileSize(const char* fname);
+static bool_t fatfsRename(const char* oldname, const char* newname);
+static bool_t fatfsOpen(GFILE* f, const char* fname);
+static void fatfsClose(GFILE* f);
+static int fatfsRead(GFILE* f, void* buf, int size);
+static int fatfsWrite(GFILE* f, const void* buf, int size);
+static bool_t fatfsSetPos(GFILE* f, long int pos);
+static long int fatfsGetSize(GFILE* f);
+static bool_t fatfsEOF(GFILE* f);
+
+static const GFILEVMT FsFatFSVMT = {
+	GFILE_CHAINHEAD,
+	GFSFLG_SEEKABLE,
+	'F',
+	fatfsDel,
+	fatfsExists,
+	fatfsFileSize,
+	fatfsRename,
+	fatfsOpen,
+	fatfsClose,
+	fatfsRead,
+	fatfsWrite,
+	fatfsSetPos,
+	fatfsGetSize,
+	fatfsEOF
+};
+
+#undef GFILE_CHAINHEAD
+#define GFILE_CHAINHEAD &FsFatFSVMT
+
+static bool_t fatfsDel(const char* fname)
+{
+
+}
+
+static bool_t fatfsExists(const char* fname)
+{
+
+}
+
+static long int fatfsFileSize(const char* fname)
+{
+
+}
+
+static bool_t fatfsRename(const char* oldname, const char* newname)
+{
+
+}
+
+static bool_t fatfsOpen(GFILE* f, const char* fname)
+{
+
+}
+
+static void fatfsClose(GFILE* f)
+{
+
+}
+
+static int fatfsRead(GFILE* f, void* buf, int size)
+{
+
+}
+
+static int fatfsWrite(GFILE* f, const void* buf, int size)
+{
+
+}
+
+static bool_t fatfsSetPos(GFILE* f, long int pos)
+{
+
+}
+
+static long int fatfsGetSize(GFILE* f)
+{
+
+}
+
+static bool_t fatfsEOF(GFILE* f)
+{
+
+}
+
