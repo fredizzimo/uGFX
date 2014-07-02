9c3935bdb409ed59889aad8b5da45302b2fb1951
inmarket <andrewh@inmarket.com.au>
2014-07-02 12:28:20 +1000

    Add eCos to gfxconf.example.h

--- gfxconf.example.h
+++ gfxconf.example.h
@@ -26,12 +26,14 @@
 ///////////////////////////////////////////////////////////////////////////
 // GOS - One of these must be defined, preferably in your Makefile       //
 ///////////////////////////////////////////////////////////////////////////
+//#define GFX_USE_OS_RAW32                             FALSE
 //#define GFX_USE_OS_CHIBIOS                           FALSE
 //#define GFX_USE_OS_FREERTOS                          FALSE
 //    #define GFX_FREERTOS_USE_TRACE                   FALSE
 //#define GFX_USE_OS_WIN32                             FALSE
 //#define GFX_USE_OS_LINUX                             FALSE
 //#define GFX_USE_OS_OSX                               FALSE
+//#define GFX_USE_OS_ECOS                              FALSE
 
 
 ///////////////////////////////////////////////////////////////////////////
