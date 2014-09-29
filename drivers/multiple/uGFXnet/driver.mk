417f64bd3ce27cc707cfd50eb1e988ddeb759fec
inmarket <andrewh@inmarket.com.au>
2014-09-29 17:50:43 +1000

    Makefile fix

--- drivers/multiple/uGFXnet/driver.mk
+++ drivers/multiple/uGFXnet/driver.mk
@@ -1,6 +1,6 @@
 GFXINC +=	$(GFXLIB)/drivers/multiple/uGFXnet
 GFXSRC +=	$(GFXLIB)/drivers/multiple/uGFXnet/gdisp_lld_uGFXnet.c
 
-ifeq($(OPT_NATIVEOS),win32)
+ifeq ($(OPT_NATIVEOS),win32)
 	GFXLIBS += ws2_32
 endif
