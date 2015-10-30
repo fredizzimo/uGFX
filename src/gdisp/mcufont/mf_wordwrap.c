69de17687c304e4699804e6644dd156099de84c6
Joel Bodenmann <joel@seriouslyembedded.com>
2015-10-30 16:40:08 +0100

    Fixing compiler warnings pull-request sent to upstream

--- src/gdisp/mcufont/mf_wordwrap.c
+++ src/gdisp/mcufont/mf_wordwrap.c
@@ -130,7 +130,6 @@ static bool append_char(const struct mf_font_s *font, int16_t width,
     }
 }
 
-static int16_t abs16(int16_t x) { return (x > 0) ? x : -x; }
 static int32_t sq16(int16_t x) { return (int32_t)x * x; }
 
 /* Try to balance the lines by potentially moving one word from the previous
@@ -173,8 +172,8 @@ static void tune_lines(struct linelen_s *current, struct linelen_s *previous,
 void mf_wordwrap(const struct mf_font_s *font, int16_t width,
                  mf_str text, mf_line_callback_t callback, void *state)
 {
-    struct linelen_s current = {};
-    struct linelen_s previous = {};
+    struct linelen_s current;
+    struct linelen_s previous;
     bool full;
     
     current.start = text;
