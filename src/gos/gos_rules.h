0313756ea38d71cfce21a0a905f4fc43ac68d197
inmarket <andrewh@inmarket.com.au>
2015-11-27 10:15:46 +1000

    Add ability to emulate malloc() and free() to prevent nasty hard to find link errors from C library routines that need malloc

--- src/gos/gos_rules.h
+++ src/gos/gos_rules.h
@@ -32,5 +32,16 @@
  	#error "GOS: GFX_FREERTOS_USE_TRACE is only available for the FreeRTOS port."
 #endif
 
+#if GFX_EMULATE_MALLOC
+	#if GFX_USE_OS_WIN32 || GFX_USE_OS_LINUX || GFX_USE_OS_OSX || GFX_USE_OS_ECOS || \
+			(GFX_OS_HEAP_SIZE == 0 && (GFX_USE_OS_RAW32 || GFX_USE_OS_ARDUINO || GFX_USE_OS_CMSIS || GFX_USE_OS_KEIL))
+		#if GFX_DISPLAY_RULE_WARNINGS
+			#warning "GOS: Cannot emulate malloc as gfxAlloc() internally uses malloc on this platform"
+		#endif
+		#undef GFX_EMULATE_MALLOC
+		#define GFX_EMULATE_MALLOC	FALSE
+	#endif
+#endif
+
 #endif /* _GOS_RULES_H */
 /** @} */
